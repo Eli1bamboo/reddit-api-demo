f51ee2a81d60c37944d48e3aac2f31b562e087e3
Elias Turrin <itwarp_eturrin@iseatz.com>
2019-12-12 12:40:20 -0300

    add pagination component

---
+++ src/components/Pagination.js
@@ -0,0 +1,179 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { getList } from '../store/actions/Actions'
+import { Grid, Button } from '@material-ui/core'
+
+class Pagination extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { pager: {} };
+    }
+
+    componentWillMount() {
+        // set page if items array isn't empty
+        if (this.props.items && this.props.items.length) {
+            this.setPage(this.props.initialPage);
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // reset page if items array has changed
+        if (this.props.items !== prevProps.items) {
+            this.setPage(this.props.initialPage);
+        }
+    }
+
+    setPage(page) {
+        var items = this.props.items;
+        var pager = this.state.pager;
+        console.log(items);
+        if (page < 1 || page > pager.totalPages) {
+            return;
+        }
+
+        // get new pager object for specified page
+        pager = this.getPager(items.length, page);
+
+        // get new page of items from items array
+        var pageOfItems = items.slice(pager.startIndex, pager.endIndex + 1);
+
+        // update state
+        this.setState({ pager: pager });
+
+        // call change page function in parent component
+        this.props.onChangePage(pageOfItems);
+    }
+
+    getPager(totalItems, currentPage, pageSize) {
+        // default to first page
+        currentPage = currentPage || 1;
+
+        // default page size is 10
+        pageSize = pageSize || 10;
+
+        // calculate total pages
+        var totalPages = Math.ceil(totalItems / pageSize);
+
+        var startPage, endPage;
+        if (totalPages <= 10) {
+            // less than 10 total pages so show all
+            startPage = 1;
+            endPage = totalPages;
+        } else {
+            // more than 10 total pages so calculate start and end pages
+            if (currentPage <= 6) {
+                startPage = 1;
+                endPage = 10;
+            } else if (currentPage + 4 >= totalPages) {
+                startPage = totalPages - 9;
+                endPage = totalPages;
+            } else {
+                startPage = currentPage - 5;
+                endPage = currentPage + 4;
+            }
+        }
+
+        // calculate start and end item indexes
+        var startIndex = (currentPage - 1) * pageSize;
+        var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+
+        // create an array of pages to ng-repeat in the pager control
+        var pages = [...Array((endPage + 1) - startPage).keys()].map(i => startPage + i);
+
+        // return object with all pager properties required by the view
+        return {
+            totalItems: totalItems,
+            currentPage: currentPage,
+            pageSize: pageSize,
+            totalPages: totalPages,
+            startPage: startPage,
+            endPage: endPage,
+            startIndex: startIndex,
+            endIndex: endIndex,
+            pages: pages
+        };
+    }
+
+    render() {
+        var pager = this.state.pager;
+
+        if (!pager.pages || pager.pages.length <= 1) {
+            // don't display pager if there is only 1 page
+            return null;
+        }
+
+        return (
+            <Grid container spacing={1}>
+                <Grid item>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        disabled={pager.currentPage === 1}
+                        onClick={() => this.setPage(1)}
+                    >
+                        First
+                    </Button>
+                </Grid>
+                <Grid item>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        disabled={pager.currentPage === 1}
+                        onClick={() => this.setPage(pager.currentPage - 1)}
+                    >
+                        Previous
+                    </Button>
+                </Grid>
+                {pager.pages.map((page, index) =>
+                    <Grid item>
+                        <Button
+                            key={index}
+                            variant="contained"
+                            color={pager.currentPage === page ? 'primary' : 'default'}
+                            onClick={() => this.setPage(page)}
+                        >
+                            {page}
+                        </Button>
+                    </Grid>
+                )}
+                <Grid item>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        disabled={pager.currentPage === pager.totalPages}
+                        onClick={() => this.setPage(pager.currentPage + 1)}
+                    >
+                        Next
+                    </Button>
+                </Grid>
+                <Grid item>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        disabled={pager.currentPage === pager.totalPages}
+                        onClick={() => this.setPage(pager.totalPages)}
+                    >
+                        Last
+                    </Button>
+                </Grid>
+            </Grid>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    const { PostsReducer: { results = [] } } = state
+
+    return {
+        results
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getList: () => dispatch(getList())
+    }
+}
+
+export default compose(connect(mapStateToProps, mapDispatchToProps))(Pagination)
